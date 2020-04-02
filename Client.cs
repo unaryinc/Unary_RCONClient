3bbbace7b55d3e924c1c91a8bd2ab7ae31bbba11
IlyaStuurFedorov <ilyastuurfedorov@gmail.com>
2020-04-02 18:42:48 +0300

    Initial commit

---
+++ Client.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LiteNetLib;
+using LiteNetLib.Utils;
+
+namespace Unary_RCONClient
+{
+    public class Client
+    {
+        public enum ConsoleColors : byte
+        {
+            Message,
+            Warning,
+            Error
+        };
+
+        private EventBasedNetListener Listener;
+        private NetManager NetClient;
+
+        public Client()
+        {
+            Listener = new EventBasedNetListener();
+
+            Listener.NetworkReceiveEvent += OnNetworkReceive;
+            Listener.PeerDisconnectedEvent += OnPeerDisconnected;
+            Listener.NetworkErrorEvent += OnNetworkError;
+
+            NetClient = new NetManager(Listener);
+            NetClient.Start();
+        }
+
+        private void OnNetworkError(System.Net.IPEndPoint endPoint, System.Net.Sockets.SocketError socketError)
+        {
+            Program.Running = false;
+        }
+
+        private void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
+        {
+            Program.Running = false;
+        }
+
+        private void OnNetworkReceive(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
+        {
+            ConsoleColors CurrentColor = (ConsoleColors)reader.GetByte();
+
+            switch(CurrentColor)
+            {
+                case ConsoleColors.Message:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case ConsoleColors.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case ConsoleColors.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+            }
+
+            Console.WriteLine(Encoding.UTF8.GetString(reader.GetRemainingBytes()));
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Send(string Command)
+        {
+            NetDataWriter Writer = new NetDataWriter();
+            Writer.Put(Encoding.UTF8.GetBytes(Command));
+            NetClient.FirstPeer.Send(Writer, DeliveryMethod.ReliableOrdered);
+        }
+
+        public void Start(string IP, int Port, string Password)
+        {
+            NetClient.Connect(IP, Port, Password);
+        }
+        
+        public void Poll()
+        {
+            NetClient.PollEvents();
+        }
+
+        public void Stop()
+        {
+            NetClient.Stop(true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Newtonsoft.Json;
+using LiteNetLib;
+using System.Threading;
+
+namespace Unary_RCONClient
+{
+    public static class Program
+    {
+        public static bool Running = true;
+
+        static void Main(string[] args)
+        {
+            Config NewConfig = JsonConvert.DeserializeObject<Config>(File.ReadAllText("Config.json"));
+            Client NewClient = new Client();
+            NewClient.Start(NewConfig.IP, NewConfig.Port, NewConfig.Password);
+
+            Thread PollThread = new Thread(() =>
+            {
+                while(Running)
+                {
+                    NewClient.Poll();
+                    Thread.Sleep(15);
+                }
+            });
+
+            PollThread.Start();
+
+            while (Running)
+            {
+                string Input = Console.ReadLine();
+
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+                Console.Write(new string(' ', Console.BufferWidth));
+                Console.SetCursorPosition(0, Console.CursorTop - 1);
+
+                if (Input == "!quit")
+                {
+                    Running = false;
+                }
+                else
+                {
+                    NewClient.Send(Input);
+                }
+            }
+
+            PollThread.Join();
+
+            NewClient.Stop();
+        }
+    }
+}
